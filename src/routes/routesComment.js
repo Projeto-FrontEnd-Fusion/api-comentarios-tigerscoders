cba53d650b4cd0cf108e7959ed67e976fec80a77
Ludimar da Silva Padilha <59611203+LudimarPadilha@users.noreply.github.com>
2024-10-28 14:21:08 -0300

    Atualização para API de novos participantes.
    Atualização para API de novos participantes.

--- src/routes/routesComment.js
+++ src/routes/routesComment.js
@@ -1,7 +1,7 @@
-const express                        = require('express');
-const router                         = express.Router();
-const{createComment, getAllComments} = require('../controllers/controllerComment');
-const validateComment                = require('../middlewares/validateComment');
+import express from 'express'
+import {createComment, getAllComments} from '../controllers/controllerComment.js'
+import validateComment from '../middlewares/validateComment.js';
+const router = express.Router();
 
 //Validando os dados da rota para criação de um comentário.
 router.post('/comments', validateComment, createComment);
@@ -10,4 +10,4 @@ router.post('/comments', validateComment, createComment);
 router.get('/comments', getAllComments);
 
 //exportando o objeto.
-module.exports = router;
+export default router;
